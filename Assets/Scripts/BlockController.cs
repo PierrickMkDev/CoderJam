fb5858f6981f8db4bdcfc6dc424c6b723b0e5a62
Paul Thibaul <Paulthb1@gmail.com>
2020-04-16 08:23:28 +0200

    add blockController

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+
+    public string horizontalAxe = "";
+    public string verticalAxe = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(horizontalAxe))
+            Debug.Log("rotation");
+        if (Input.GetButtonDown(verticalAxe))
+            Debug.Log("scale");
+    }
+}
